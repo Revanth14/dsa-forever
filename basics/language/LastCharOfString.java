403fe3024d96248fd2d891e739fc73e8ee2ea5c7
Revanth Ch <revanthcse1@gmail.com>
2026-01-16 17:30:47 -0500

    LastCharOfString.java

---
+++ basics/language/LastCharOfString.java
@@ -0,0 +1,14 @@
+package language;
+import java.util.*;
+
+public class LastCharOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        System.out.println(str.charAt(str.length()-1));
+
+        sc.close();
+        
+    }
+}
