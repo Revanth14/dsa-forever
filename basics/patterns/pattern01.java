a2f1019cb102e2390edf03d891d8b0541a45f7d2
Revanth Ch <revanthcse1@gmail.com>
2026-01-18 20:37:40 -0500

    new pattern

--- basics/patterns/pattern01.java
+++ basics/patterns/pattern01.java
@@ -1,13 +1,13 @@
 package patterns;
 
 import java.util.Scanner;
-public class pattern01 {
+public class Pattern01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt(); // Number of rows
 
         for (int i = 1; i <= n; i++) { // Loop for each row
-            for (int j = 1; j <= i; j++) { // Loop for printing stars in each row
+            for (int j = 1; j <n; j++) { // Loop for printing stars in each row
                 System.out.print("* ");
             }
             System.out.println(); // Move to the next line after each row
@@ -16,3 +16,13 @@ public class pattern01 {
     }
     
 }
+/*
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+
+*/
