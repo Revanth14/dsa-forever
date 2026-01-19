9fd75a5f5184936b883bac1f99bf233405654c00
Revanth Ch <revanthcse1@gmail.com>
2026-01-18 20:38:06 -0500

    pattern-2

---
+++ basics/patterns/Pattern02.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.*;;
+
+public class Pattern02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Number of rows
+
+        for (int i = 0; i<n;i++){
+            for (int j=0; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
+/*
+*
+**
+***
+****
+*****
+******
+*******
+*/
