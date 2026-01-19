4824ac1603254a042e3da49d6d400b02d17bb69a
Revanth Ch <revanthcse1@gmail.com>
2026-01-18 20:46:42 -0500

    pattern 10

---
+++ basics/patterns/Pattern10.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class Pattern10 {
+    
+}
+
+    private static void main(String[] args) {
+        for (int i = 0; i<n;i++){
+            for (int j=0; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+private static void main(String[] args) {
+
+        for (int i = 1; i<=n;i++){
+            for (int j=1; j<=(n-i+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
