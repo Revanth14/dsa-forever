78766c5b2555c964c5524143cb83586482c4acde
Revanth Ch <revanthcse1@gmail.com>
2026-01-20 17:44:27 -0500

    pattern-22 concentric squares number pattern

---
+++ basics/patterns/Pattern22.java
@@ -0,0 +1,47 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // Outer loop for the rows
+        for (int i = 0; i < 2 * n - 1; i++) {
+            // Inner loop for the columns
+            for (int j = 0; j < 2 * n - 1; j++) {
+                
+                /* Initialising the top, down, left
+                and right indices of a cell.*/
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
+                
+                /* Minimum of 4 directions and then we 
+                subtract from n because previously we 
+                would get a pattern whose border has 0's, 
+                but we want with border n's and then
+                decreasing inside.*/
+                System.out.print((n - Math.min(Math.min(top, bottom), Math.min(left, right))) + " ");
+            }
+            // Move to the next row
+            System.out.println();
+        }
+        sc.close();
+        
+    }
+    
+}
+
+/*
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4 
+4 3 2 2 2 3 4 
+4 3 3 3 3 3 4 
+4 4 4 4 4 4 4 
+
+*/
