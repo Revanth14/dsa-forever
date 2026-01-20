26a9a6880827a69f71d38fd1e696e70cd05d1646
Revanth Ch <revanthcse1@gmail.com>
2026-01-20 17:41:47 -0500

    pattern-19 hollow diamond

---
+++ basics/patterns/Pattern19.java
@@ -0,0 +1,77 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+         int iniS = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            
+            for (int j = 0; j < iniS; j++) {
+                System.out.print(" ");
+            }
+            
+            // Printing the stars in the row.
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            
+            /* The spaces increase by 2 
+            every time we hit a new row. */
+            iniS += 2;
+            
+            // Give a line break for a new row.
+            System.out.println();
+        }
+        
+        // Print the lower half pattern
+        
+        // Store the initial spaces.
+        iniS = 2 * n - 2;
+        
+        for (int i = 1; i <= n; i++) {
+            // Printing the stars in the row.
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            
+            // Printing the spaces in the row.
+            for (int j = 0; j < iniS; j++) {
+                System.out.print(" ");
+            }
+            
+            // Printing the stars in the row.
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            
+            /* The spaces decrease by 2 
+            every time we hit a new row. */
+            iniS -= 2;
+            
+            // Give a line break for a new row.
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
+
+/*
+********
+***  ***
+**    **
+*      *
+*      *
+**    **
+***  ***
+********
+
+ */
